b256d3f9344d8a24798002efc4cac6e7df428f80
Dilshodbek200207 <d63819089@gmail.com>
2025-02-23 14:56:16 +0500

    first commit from local

---
+++ demoapp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello World");
+Console.WriteLine("Hello Dilshodbek");
+Console.WriteLine("Hello Diyor");
+Console.WriteLine("Hello Fazliddin");
+Console.WriteLine("Hello Firdavs");
+Console.WriteLine("Hello Sunnat");
